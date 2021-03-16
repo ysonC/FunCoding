841481c0acea8ceffcb1a22152cba37b338428c5
Wyson <efyhc5@nottingham.ac.uk>
2021-03-16 13:45:46 +0800

    11730 uncomplete

--- FunCoding/C++/NTHU/11730.cpp
+++ FunCoding/C++/NTHU/11730.cpp
@@ -2,23 +2,32 @@
 
 using namespace std;
 
+// int counter(int a)
+// {
+//    return a++;
+// }
 
-void diffLenghtChar(char correct[100], char check[100])
+int diffLenghtChar(char correct[100], char check[100])
 {
     int correctCnt = 0;
     int checkCnt = 0;
+    int totalCnt = 0;
     for (int i = 0; correct[i] != '\0'; i++)
         correctCnt++; 
     for (int i = 0; check[i] != '\0'; i++)
         checkCnt++;
     if (correctCnt != checkCnt)
-        cout << "Different length" << endl;
-    else
-        cout << "Same length" << endl;
+    {
+        //cout << "Different length" << endl;
+        totalCnt++;
+    }
+    // else
+    //     cout << "Same length" << endl;
     
+    return totalCnt;
 }
 
-void wrongChar(char correct[100], char check[100])
+int wrongChar(char correct[100], char check[100])
 {
     int cnt = 0;
     for(int i=0; i<100 && correct[i]!='\0'; i++)
@@ -28,27 +37,29 @@ void wrongChar(char correct[100], char check[100])
 
             if (correct[i+1] == check[i+1])
             {
-                cout << "One wrong char" << endl;
+                //cout << "One wrong char" << endl;
                 cnt++;
                 break;
             }
-            else if (correct[i] == check[i+1])
+            else if (correct[i] == check[i+1] && correct[i+1] == check[i])
             {
-                cout << "Switch place Char" << endl;
+                //cout << "Switch place Char" << endl;
                 cnt++;
                 break;
             }
             else 
             {
-                cout << "More than one wrong char" << endl;
-                cnt++;
+                //cout << "More than one wrong char" << endl;
+                cnt += 2;
                 break;
             }
         }
         
     }
-    if (cnt == 0)
-        cout << "All same char" << endl;
+    // if (cnt == 0)
+    //     cout << "All same char" << endl;
+
+    return cnt;
 }
 
 
@@ -57,12 +68,20 @@ int main()
     char correct[100];
     char input[100];
     int t;
+    int rightOrWrong;
     cin >> t;
     while (t--)
     {
         cin >> correct >> input;
-        diffLenghtChar(correct,input);
-        wrongChar(correct,input);
+        
+        rightOrWrong = diffLenghtChar(correct,input) + wrongChar(correct,input);
+        //cout << rightOrWrong << endl;
+        //diffLenghtChar(correct,input);
+        //wrongChar(correct,input);
+        if (rightOrWrong > 1 || rightOrWrong == 0)
+            cout << "No" << endl;
+        else
+            cout<< "Yes" << endl;
     }
 
 }
--- FunCoding/C++/RandomStuff/test_run2.cpp
+++ FunCoding/C++/RandomStuff/test_run2.cpp
@@ -2,43 +2,20 @@
 #include<math.h>
 using namespace std;
 
+
+int counter (int a)
+{
+    a++;
+    return a;
+}
+
 int main ()
 {
-    char a[3];
-    int n, tmp, ans, b;
-    char store[10];
-    ans = 0;
-    cin.getline(a,5);
-    cin >> n;
-    tmp = 0;
+    int a, b;
+    int cnt = 0;
+    cin >> a >> b;
+    if (a > b)
+         cnt = counter(cnt);
 
-    while(n--)//!!! not cnt<n
-    {
-        cin >> store; // enter test value ex. 1212 ==> 2
-        for (int i = 0; i < 10; i++)
-        {
-            for(int j =0; j <= 3; j++)
-            {
-                if (store[i] == a[j])
-                {
-                    if (store[i+1] == a[j+1])
-                    {
-                        //cout << "found 12" << endl;
-                        tmp++;
-                    }
-                    else
-                    break;
-                }
-                else 
-                break;
-            }
-
-        }
-            if (tmp > ans)
-            ans = tmp;
-            tmp = 0;
-    }
-
-    cout << "ans is : " << ans;
-
+    cout << cnt;
 }
