44fcebbb12e1630ed518057f00b6158169caf4a5
Wyson <efyhc5@nottingham.ac.uk>
2021-03-17 17:35:53 +0800

    modified

--- FunCoding/C++/NTHU/11730.cpp
+++ FunCoding/C++/NTHU/11730.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //    return a++;
 // }
 
-int diffLenghtChar(char correct[100], char check[100])
+bool diffLenghtChar(char correct[100], char check[100])
 {
     int correctCnt = 0;
     int checkCnt = 0;
@@ -18,13 +18,19 @@ int diffLenghtChar(char correct[100], char check[100])
         checkCnt++;
     if (correctCnt != checkCnt)
     {
-        //cout << "Different length" << endl;
+        //cout << "Different length" << endl; // return true 
         totalCnt++;
     }
     // else
-    //     cout << "Same length" << endl;
-    
-    return totalCnt;
+    //     cout << "Same length" << endl; // return false
+
+    //cout << "diffCnt = " << totalCnt << endl;
+
+    if (totalCnt == 1)
+        return true;
+    else
+        return false;
+
 }
 
 int wrongChar(char correct[100], char check[100])
@@ -35,16 +41,22 @@ int wrongChar(char correct[100], char check[100])
         if (correct[i] != check[i])
         {
 
-            if (correct[i+1] == check[i+1])
+            if (correct[i+1] == check[i+1] && diffLenghtChar(correct,check) == false)
             {
                 //cout << "One wrong char" << endl;
                 cnt++;
                 break;
             }
-            else if (correct[i] == check[i+1] && correct[i+1] == check[i])
+            else if (correct[i] == check[i+1] && correct[i+1] == check[i] && diffLenghtChar(correct,check) == false)
             {
                 //cout << "Switch place Char" << endl;
                 cnt++;
+                break;
+            }
+            else if (diffLenghtChar(correct,check) == true && correct[i] == check[i+1] || diffLenghtChar(correct,check) == true)
+            {
+                cnt++;
+                //cout << "Missing one" << endl;
                 break;
             }
             else 
@@ -54,10 +66,14 @@ int wrongChar(char correct[100], char check[100])
                 break;
             }
         }
-        
     }
     // if (cnt == 0)
     //     cout << "All same char" << endl;
+
+    if (cnt == 0 && diffLenghtChar(correct,check) == true)
+    {
+        cnt++;
+    }
 
     return cnt;
 }
@@ -74,14 +90,17 @@ int main()
     {
         cin >> correct >> input;
         
-        rightOrWrong = diffLenghtChar(correct,input) + wrongChar(correct,input);
+        //rightOrWrong = diffLenghtChar(correct,input) + wrongChar(correct,input);
+
+        rightOrWrong = wrongChar(correct,input);
+
         //cout << rightOrWrong << endl;
-        //diffLenghtChar(correct,input);
-        //wrongChar(correct,input);
+        //cout << rightOrWrong << endl;
+        //cout << diffLenghtChar(correct,input) << endl;
+
         if (rightOrWrong > 1 || rightOrWrong == 0)
             cout << "No" << endl;
         else
             cout<< "Yes" << endl;
     }
-
 }
