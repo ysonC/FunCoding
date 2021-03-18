db09321d5fa881a3d436b70ca8dd41f481122d97
Wyson <efyhc5@nottingham.ac.uk>
2021-03-18 17:01:54 +0800

    modified test_run

--- FunCoding/Haskell/test_run.hs
+++ FunCoding/Haskell/test_run.hs
@@ -26,3 +26,17 @@ insert x (y:ys) = if x <= y then
                     x:y:ys
                     else
                         y:insert x ys
+
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) = if x <= y then
+                        x : merge xs (y:ys)
+                        else
+                            y : merge (x:xs) ys
+
+-- msort :: [Int] -> [Int]
+-- msort [] = []
+-- msort [x] = [x]
+-- msort xs = merge (msort ys) (msort zs)
+--             where (ys,zs) = halve xs
